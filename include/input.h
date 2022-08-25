48935ff49b828a72af83bfeb4373639810a0c0b6
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-08-25 17:36:55 +0800

    Add parseFASTAQ method

--- include/input.h
+++ include/input.h
@@ -4,21 +4,20 @@
 #include <vector>
 #include <memory>
 
+#include "bioparser/parser.hpp"
+#include "biosoup/nucleic_acid.hpp"
 #include "overlap.h"
 
 namespace PAFa {
 class Input {
   public:
-
     explicit Input(const std::string &path);
-    std::vector<std::unique_ptr<Overlap>> Sequences();
+    std::vector<std::unique_ptr<Overlap>> ParsePAF();
+    std::vector<std::unique_ptr<biosoup::NucleicAcid>> ParseFASTAQ();
 
   private:
-
-    static bool IsSuffix(const std::string &s, const std::string &suff);
-    void ParseFile();
+    static bool IsSuffix(const std::string &s, const std::string &suffix);
     const std::string &path_;
-    std::vector<std::unique_ptr<Overlap>> overlaps_;
 };
 } // namespace PAFa
 
--- src/input.cpp
+++ src/input.cpp
@@ -1,5 +1,7 @@
 #include "../include/input.h"
 
+#include "bioparser/fasta_parser.hpp"
+#include "bioparser/fastq_parser.hpp"
 #include "bioparser/parser.hpp"
 #include "bioparser/paf_parser.hpp"
 
@@ -8,29 +10,45 @@
 
 namespace PAFa{
 Input::Input(const std::string &path) :
-        path_(path) {
-    ParseFile();
-}
-
-void Input::ParseFile() {
-    if (IsSuffix(path_, ".paf")) {
-        try {
-            auto parser = bioparser::Parser<Overlap>::Create<bioparser::PafParser>(path_);
-            overlaps_ = parser->Parse(-1);
-            return;
-        } catch (const std::invalid_argument &exception) {
-            std::cerr << exception.what() << std::endl;
-            return;
-        }
-    }
-
-    throw std::invalid_argument("Error: Invalid PAF file format.");
-}
-
+        path_(path) {}
 bool Input::IsSuffix(const std::string &s, const std::string &suffix) {
     return s.size() >= suffix.size() && s.compare(s.size() - suffix.size(), suffix.size(), suffix) == 0;
 }
-std::vector<std::unique_ptr<Overlap>> Input::Sequences() {
-    return std::move(overlaps_);
+std::vector<std::unique_ptr<Overlap>> Input::ParsePAF() {
+    if (IsSuffix(path_, ".paf")) {
+        try {
+            auto parser = bioparser::Parser<Overlap>::Create<bioparser::PafParser>(path_);
+            auto overlaps = parser->Parse(-1);
+            return overlaps;
+        } catch (const std::invalid_argument &exception) {
+            std::cerr << exception.what() << std::endl;
+        }
+    }
+
+    throw std::invalid_argument("Error: Invalid file format.");
+}
+std::vector<std::unique_ptr<biosoup::NucleicAcid>> Input::ParseFASTAQ() {
+    if (IsSuffix(path_, ".fasta") || IsSuffix(path_, ".fasta.gz") ||
+            IsSuffix(path_, ".fna") || IsSuffix(path_, ".fna.gz") ||
+            IsSuffix(path_, ".fa") || IsSuffix(path_, ".fa.gz")) {
+        try {
+            auto parser = bioparser::Parser<biosoup::NucleicAcid>::Create<bioparser::FastaParser>(path_);
+            auto sequences = parser->Parse(-1);
+            return sequences;
+        } catch (const std::invalid_argument &exception) {
+            std::cerr << exception.what() << std::endl;
+        }
+    }
+    if (IsSuffix(path_, ".fastq") || IsSuffix(path_, ".fastq.gz") ||
+            IsSuffix(path_, ".fq") || IsSuffix(path_, ".fq.gz")) {
+        try {
+            auto parser = bioparser::Parser<biosoup::NucleicAcid>::Create<bioparser::FastqParser>(path_);
+            auto sequences = parser->Parse(-1);
+            return sequences;
+        } catch (const std::invalid_argument &exception) {
+            std::cerr << exception.what() << std::endl;
+        }
+    }
+    throw std::invalid_argument("Error: Invalid sequences file format.");
 }
 } // namespace PAFa
